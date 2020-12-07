0c14f4620d6c42f4e4a5f0e2df79410515765b89
Igorth <igordiasth@gmail.com>
2020-12-08 10:35:19 +1300

    creating first component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const AboutPage = () => (
-  <>
-  <h1>About page</h1>
-  <ul>
-      <li>
-        <a href="/about">About</a>
-      </li>
-      <li>
-        <Link to="/" activeStyle={{ color: 'red'}}>Home</Link>
-      </li>
-      <li>
-        <Link to="/about" activeStyle={{ color: 'red'}}>About Link</Link>
-      </li>
-    </ul>
-  </>
+  <Layout>
+    <SEO title="About" />
+    <h1>About page</h1>
+  </Layout>
 )
 
 export default AboutPage
