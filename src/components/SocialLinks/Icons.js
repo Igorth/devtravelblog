00c86fef647dc87ea9734d60a1b688b0867011fd
Igorth <igordiasth@gmail.com>
2020-12-11 08:50:21 +1300

    styled social icons

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,15 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+import { Codepen } from "@styled-icons/boxicons-logos/Codepen"
+import { Web } from "@styled-icons/foundation/Web"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
+
+const Icons = {
+  Github,
+  Linkedin,
+  Codepen,
+  Web,
+  Instagram,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/Igorth`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/igordiasth/`,
+  },
+  {
+    label: "Codepen",
+    url: `https://codepen.io/igordiasth`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/uaiboraviajar`,
+  },
+  {
+    label: "Web",
+    url: `https://igordiasth.dev`,
+  },
+]
+
+export default links
